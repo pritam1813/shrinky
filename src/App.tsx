f06f8067fa1e6e4303608301c1acdd9bdcc84c45
Pritam Dhara <pritam1813@gmail.com>
2023-09-05 13:31:24 +0530

    Corrected Wrong React Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "papercss/dist/paper.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup, About, Pricing } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/pricing' element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
-import { Signup, About, Pricing } from "./pages";
-import Navbar from "./components/Navbar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/pricing' element={<Pricing />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
