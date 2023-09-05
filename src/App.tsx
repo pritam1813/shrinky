bf54da17753aa4e8ef3a94e6ab4a57ca7ba361e3
Pritam Dhara <pritam1813@gmail.com>
2023-09-05 13:55:11 +0530

    corrected React router dom setup again

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import "papercss/dist/paper.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Signup, About, Pricing } from "./pages";
-import { Navbar } from "./components";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/pricing' element={<Pricing />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "papercss/dist/paper.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup, About, Pricing } from "./pages";
+import { Navbar } from "./components";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/pricing' element={<Pricing />} />
+    </Routes>
+  </BrowserRouter>
 );
