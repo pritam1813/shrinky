3fe3d94b40ef547d843ca9810a2b66716f587fc3
Pritam Dhara <pritam1813@gmail.com>
2023-09-05 14:18:32 +0530

    Added Meta Description

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Signup, About, Pricing } from "./pages";
+import { Navbar } from "./components";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/pricing' element={<Pricing />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "papercss/dist/paper.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Signup, About, Pricing } from "./pages";
-import { Navbar } from "./components";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/pricing' element={<Pricing />} />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
